d5d616b97177c4a64a5580a20656b32ccec47254
DiamondArrowTheLizard <diamond1arrow@gmail.com>
2026-05-03 18:47:14 +0700

    Parser: started from scratch

--- Cli/Program.cs
+++ Cli/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Core.Lexer;
-using Core.Parser;
 using System;
 using System.IO;
 using System.Linq;
@@ -15,26 +14,3 @@ while ((line = Console.ReadLine()) != null)
 var csv = new LexerCsv();
 csv.Build(lexer);
 csv.WriteToStream(Console.Out);
-
-Console.WriteLine();
-Console.WriteLine("--- Парсер ---");
-
-var parser = new Parser();
-var errors = parser.Parse(lexer.Nodes.ToList());
-
-if (errors.Count == 0)
-{
-    Console.WriteLine("Без ошибок.");
-}
-else
-{
-    Console.WriteLine($"Найдено ошибок: {errors.Count}");
-    Console.WriteLine();
-    Console.WriteLine($"{"Неверный фрагмент",-20} {"Местоположение",-15} Описание ошибки");
-    Console.WriteLine(new string('-', 80));
-    foreach (var err in errors)
-    {
-        string location = $"Строка {err.Line}, Нач {err.Column}";
-        Console.WriteLine($"{err.InvalidFragment,-20} {location,-15} {err.Description}");
-    }
-}
--- Core/Parser/ErrorInfo.cs
+++
@@ -1,8 +0,0 @@
-namespace Core.Parser;
-
-public record ErrorInfo(
-    string InvalidFragment,
-    uint Line,
-    uint Column,
-    string Description
-);
